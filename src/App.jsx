215fbfeb85aca1b098a9f67bae850fc46bcb1c39
Abdullah-4u <abdullahmollah311@gmail.com>
2025-03-31 22:08:08 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense, useContext} from "react";
+import { lazy, Suspense, useContext, useEffect} from "react";
 import { AppContext } from "./context/AppContext";
 import { AnimatePresence } from "framer-motion";
 import Loader from "./components/Loader";
@@ -14,6 +14,11 @@ function App() {
   const lenis = new Lenis({
     autoRaf: true,
   });
+
+  useEffect(() => {
+    document.body.style.overflow = orderConfirmationStatus ? "hidden" : "";
+    return () => (document.body.style.overflow = "");
+  }, [orderConfirmationStatus]);
 
   return (
     <div className="w-full max-w-screen-xl mx-auto bg-rose-100 flex lg:flex-row flex-col items-center sm:items-start pb-8">
